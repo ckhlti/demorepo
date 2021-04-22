c846c8741286d0111680de652eb61cfc52b39058
vbrk1971-lti <ravikumar.balasubramanian@lntinfotech.com>
2021-04-22 18:06:51 -0400

    fixing cta/clc/intro-card

--- lti-coe/ui.apps/src/main/content/jcr_root/apps/coe/components/core/custom-layout-container/v1/custom-layout-container/clientlibs/js/container.js
+++ lti-coe/ui.apps/src/main/content/jcr_root/apps/coe/components/core/custom-layout-container/v1/custom-layout-container/clientlibs/js/container.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
-   applyLayoutContainerClass();
-});
-
-$(window).resize(function() {
-   applyLayoutContainerClass();
-});
-
-function applyLayoutContainerClass() {
-    console.log("test");
-    $('.custom-layout-container-parameters').each(function(){
-        var componentID, desktopClass, mobileClass;
-        componentID ="#" + $(this).find('.componentclass').attr('value');
-        desktopClass =$(this).find('.desktopclass').attr('value');
-        mobileClass =$(this).find('.mobileClass').attr('value');
-
-        
-        if($(window).width() <= 896) {
-            $(componentID).removeClass(desktopClass).addClass(mobileClass);
-        }
-        else {
-            $(componentID).removeClass(mobileClass).addClass(desktopClass);
-        }
-
-    });
-}
+    applyLayoutContainerClass();
+ });
+ 
+ $(window).resize(function() {
+    applyLayoutContainerClass();
+ });
+ 
+ function applyLayoutContainerClass() {
+     $('.custom-layout-container-parameters').each(function(){
+         var componentID, desktopClass, mobileClass;
+         componentID ="#" + $(this).find('.componentclass').attr('value');
+         desktopClass =$(this).find('.desktopclass').attr('value');
+         mobileClass =$(this).find('.mobileClass').attr('value');
+ 
+         var divObject = $(componentID);
+         if (divObject.attr("Data-click-target") >= "")
+         {
+             divObject.click(function() {window.location.href=divObject.attr("Data-click-target")});
+             divObject.css("cursor","pointer");
+         }
+         
+         if($(window).width() <= 896) {
+             $(componentID).removeClass(desktopClass).addClass(mobileClass);
+         }
+         else {
+             $(componentID).removeClass(mobileClass).addClass(desktopClass);
+         }
+ 
+     });
+ }
