a333ffe1510b89a6566b8a742ace7adef2c2fd70
Sonal Sahu <sonal.sahu@lntinfotech.com>
2021-04-23 17:19:12 +0530

    Adding Accordion card V2 Component

---
+++ lti-coe/ui.apps/src/main/content/jcr_root/apps/coe/components/core/accordion-card/v2/accordion-card/clientlibs/js/card.js
@@ -0,0 +1,27 @@
+$(".coe-toggler").each(function() {
+
+   //console.log($(this).html());
+    var id = $(this).attr("id");
+    console.log(id);
+    $("#"+id).find(".down-arrow").click(function() {toggleDetailsCard(id);});
+    $("#"+id).find(".up-arrow").click(function() {toggleSummaryCard(id);});
+}
+)
+function toggleDetailsCard(cardId)
+{
+                var togglerId = $("#"+cardId);;
+     togglerId.find(".toggler-contents .toggler-details").show("slow");
+    togglerId.find(".toggler-contents .toggler-summary").hide();
+    togglerId.find(".toggler-cta .down-arrow").hide();
+    togglerId.find(".toggler-cta .up-arrow").show();
+}
+
+function toggleSummaryCard(cardId)
+{
+      var togglerId = $("#"+cardId);
+    $(togglerId).find(".toggler-contents .toggler-details").hide();
+    $(togglerId).find(".toggler-contents .toggler-summary").show();
+    $(togglerId).find(".toggler-cta .down-arrow").show();
+    $(togglerId).find(".toggler-cta .up-arrow").hide();
+
+}
