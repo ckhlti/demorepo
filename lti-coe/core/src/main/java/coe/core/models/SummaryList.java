3a58fe8cb9f6e9ec4551203f896c5c675b79176b
ck143 <ck143@DESKTOP-RHDVJR2>
2021-04-27 13:27:16 +0530

    feature-summarylist
    Added summarylist component and updated clientlib base embed with
    summarylist clientlinb

---
+++ lti-coe/core/src/main/java/coe/core/models/SummaryList.java
@@ -0,0 +1,73 @@
+package coe.core.models;
+
+public class SummaryList {
+
+	private String title;
+	private String image;
+	private String shortDescription;
+	private String path;
+	
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image
+	 *            the image to set
+	 */
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return the shortDescription
+	 */
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	/**
+	 * @param shortDescription
+	 *            the shortDescription to set
+	 */
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	
+	/**
+	 * @return the path
+	 */
+	public String getPath() {
+		return path;
+	}
+
+	/**
+	 * @param path
+	 *            the path to set
+	 */
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
+
