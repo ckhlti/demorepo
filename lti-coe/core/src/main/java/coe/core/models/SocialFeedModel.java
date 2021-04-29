d3d390c1c815ce117cc3843beadcf3d617c4986c
10673746 <akankshya.mishra@lntinfotech.com>
2021-04-29 21:12:23 +0530

    added Social feed component to share url,title

--- lti-coe/core/src/main/java/coe/core/models/SocialFeedModel.java
+++ lti-coe/core/src/main/java/coe/core/models/SocialFeedModel.java
@@ -8,16 +8,80 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.engine.SlingSettingsService;
+import org.apache.sling.api.resource.PersistenceException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Optional;
+import coe.core.utils.GlobalUtil;
 
 import javax.inject.Inject;
-import javax.inject.Named;
-
-import java.util.*;
+//import javax.inject.Named;
+//import java.util.*;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class SocialFeedModel {
 	private static final Logger log = LoggerFactory.getLogger(SocialFeedModel.class);
     
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+    @Default(values="No resourceType")
+    protected String resourceType;
+    
+    @OSGiService
+    private SlingSettingsService settings;
+    @SlingObject
+    private Resource currentResource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
+ 
+    @PostConstruct
+    protected void init() {
+        if (StringUtils.isEmpty(componentId) && currentResource != null) {
+ 
+            componentId = GlobalUtil.generateUniqueHashId("Social-Feed", currentResource.getPath());     //Generate unique component Id
+ 
+            log.debug("componentId generated :: path:: componentId {} , {}", currentResource.getPath(), componentId);
+ 
+            if (StringUtils.isNotEmpty(componentId)) {
+ 
+                try {
+                    
+                    Map<String,String> propertyMap = new HashMap<>();
+                    
+                    propertyMap.put("componentId", componentId);
+                    
+                    GlobalUtil.customPropertyWriter(currentResource.getResourceResolver(),currentResource.getPath(),propertyMap);
+                    
+                } catch (PersistenceException ex) {
+ 
+                    log.error("Exception occured while Saving value in the node :: {}", ex);
+ 
+                }
+            }else {
+                
+                log.error("Unique Id Can not be Written Social Feed Model ");
+                
+            }
+        }
+        else
+        {
+            log.info("Component id " + componentId);
+            log.info("resource - " + currentResource.getPath().toString());
+            log.info("Component id not generated");
+        }    
+    }
+
+    @ValueMapValue
+    private String componentId;
+
     @Inject
     private String socialurl;
  
@@ -32,9 +96,6 @@ public class SocialFeedModel {
 
     @Inject
     private String fb;
-
-    @Inject
-    private String insta;
 
     @Inject
     private String twitter;
@@ -66,10 +127,6 @@ public class SocialFeedModel {
         return fb;
     }
 
-    public String getInsta() {
-        return insta;
-    }
-
     public String getTwitter() {
         return twitter;
     }
@@ -78,4 +135,7 @@ public class SocialFeedModel {
         return linkedin;
     }
 
+    public String getComponentid(){
+        return componentId;
+    }
 }
---
+++ lti-coe/ui.apps/src/main/content/jcr_root/apps/coe/components/core/socialfeed/v1/socialfeed/clientlib/js/script.js
@@ -0,0 +1,14 @@
+/*function share_fb() {
+    console.log("${social.socialurl @ context='html'}");
+    window.open('https://www.facebook.com/sharer/sharer.php?u='+"${social.socialurl @ context='html'}&quote="+"${social.socialtitle @context='html'}",'facebook-share-dialog',"width=626, height=436")
+}
+
+function share_twitter() {
+    console.log("${social.socialurl @ context='html'}");
+    window.open('http://twitter.com/share?url='+"${social.socialurl @ context='html'}&title="+"${social.socialtitle @context='html'}",'_blank', 'width=550,height=420')
+}
+
+function share_linkedin() {
+    console.log("${social.socialurl @ context='html'}");
+    window.open('https://www.linkedin.com/sharing/share-offsite/?url='+"${social.socialurl @ context='html'}",'left=20,top=20,width=500,height=500,toolbar=1,resizable=0')
+}*/
