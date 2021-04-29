6022d6f1bb4b94000cae428438937f3260d88489
Aditya8and <aditya.band@lntinfotech.com>
2021-04-29 15:43:26 +0530

    Added component Id and unique class

--- lti-coe/core/src/main/java/coe/core/models/ClickableImageModel.java
+++ lti-coe/core/src/main/java/coe/core/models/ClickableImageModel.java
@@ -1,10 +1,28 @@
 package coe.core.models;
 
-import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletRequest; 
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
+import javax.annotation.PostConstruct;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.api.resource.PersistenceException;
+import javax.inject.Inject;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.settings.SlingSettingsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import java.util.HashMap;
+import java.util.Map;
+import coe.core.utils.GlobalUtil;
 //@Model(adaptables=SlingHttpServletRequest.class)
 //public class ClickableImageModel {
 //	@Inject
@@ -23,6 +41,16 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 @Model(adaptables = SlingHttpServletRequest.class,
 defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ClickableImageModel {
+	private static final Logger LOG = LoggerFactory.getLogger(ClickableImageModel.class);
+
+@SlingObject
+private Resource currentResource;
+
+@ValueMapValue @Optional
+private String componentId;
+
+@ValueMapValue @Optional
+private String uniqueClassName;
 
  @ValueMapValue
 private String altText;
@@ -35,6 +63,43 @@ private String linkTarget;
 
 @ValueMapValue
 private String fileReference;
+
+
+@PostConstruct
+protected void init() {
+	if (StringUtils.isEmpty(componentId) && currentResource != null) {
+
+		componentId = GlobalUtil.generateUniqueHashId("ClickableImage", currentResource.getPath()); 	//Generate unique component Id
+
+
+		if (StringUtils.isNotEmpty(componentId)) {
+
+			try {
+				
+				Map<String,String> propertyMap = new HashMap<>();
+				
+				propertyMap.put("componentId", componentId);
+				
+				GlobalUtil.customPropertyWriter(currentResource.getResourceResolver(),currentResource.getPath(),propertyMap);
+				
+			} catch (PersistenceException ex) {
+
+				LOG.error("Exception occured while Saving value in the node :: {}", ex);
+
+			}
+		}else {
+			
+			LOG.error("Unique Id Can not be Written Custom Carousel Model ");
+			
+		}
+	}
+	else
+	{
+		LOG.info("Component id for Custom Carousel" + componentId);
+		LOG.info("Component id not generated");
+	}    
+}
+
 
 public String getAltText() {
 	return altText;
@@ -50,6 +115,16 @@ public String getLinkTarget() {
 
 public String getFileReference() {
 	return fileReference;
+}
+
+
+public String getComponentId() {
+	return componentId;
+}
+
+
+public String getUniqueClassName() {
+	return uniqueClassName;
 }
 
 
